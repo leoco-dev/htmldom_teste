aba2b4321d7a9c676613780bf2ba540a52e21cd7
Leonardo <leo.co@live.com>
2023-04-21 10:03:42 -0300

    feat: validação números iguais

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 const form = document.querySelector('#form-comparar');
 
+const valorMenor = document.querySelector('#valor-menor');
+const valorMaior = document.querySelector('#valor-maior');
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+
+    limparResultado()
+    comparaNumeroIgual()
+    comparaNumero()
+})
+
+function comparaNumeroIgual() {    
+
+    const comparaNumeroIgual = parseInt(valorMaior.value) == parseInt(valorMenor.value);
     
-    document.querySelector('.sucess-msg').style.display = 'none';
-    document.querySelector('.error-msg').style.display = 'none';
+    const msgSimilar = `Não foi possível validar, os números: <b>${valorMenor.value}</b> e <b>${valorMaior.value}</b> são iguais.`;
+
+    if(comparaNumeroIgual) {
         
-    let valorMenor = document.querySelector('#valor-menor');
-    let valorMaior = document.querySelector('#valor-maior');
-    const maiorNumber =  parseInt(valorMaior.value);
-    const menorNumber =  parseInt(valorMenor.value);
-    const comparaNumero = maiorNumber > menorNumber;
+        let containerMsgSimilar = document.querySelector('.similar-msg');
+        containerMsgSimilar.innerHTML = msgSimilar;
+        containerMsgSimilar.style.display = 'inline';
+
+        valorMaior.value = "";
+        valorMenor.value = "";
+
+    }
+}
+
+function comparaNumero() {
+
+    const comparaNumero = parseInt(valorMaior.value) > parseInt(valorMenor.value);
+        
     const msgSucesso = `Está certo, muito bem! <b>${valorMenor.value}</b> é menor que <b>${valorMaior.value}</b>`;
     const msgError = `Ops, tente novamente! <b>${valorMenor.value} não </b> é menor que <b>${valorMaior.value}</b>`;
 
@@ -23,11 +45,18 @@ form.addEventListener('submit', function(e) {
         valorMenor.value = "";
 
     } else {
-        let containerMsgSucess = document.querySelector('.error-msg');
-        containerMsgSucess.innerHTML = msgError;
-        containerMsgSucess.style.display = 'inline';
+        let containerMsgError = document.querySelector('.error-msg');
+        containerMsgError.innerHTML = msgError;
+        containerMsgError.style.display = 'inline';
 
         valorMaior.value = "";
         valorMenor.value = "";
     }
-})
+}
+
+function limparResultado() {
+    
+    document.querySelector('.sucess-msg').style.display = 'none';
+    document.querySelector('.error-msg').style.display = 'none';
+    document.querySelector('.similar-msg').style.display = 'none';
+}
